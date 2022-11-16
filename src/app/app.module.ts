09e36b4410511045b39839d932513750632d5b4a
Karnchai-Van <Sakkarnjana@outlook.com>
2022-11-16 10:44:19 +0700

    Setting up dependency project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header/header.component';
+import { DisciplineComponent } from './discipline/discipline/discipline.component';
+import { StatusComponent } from './status/status.component';
+import { AchivedComponent } from './achived/achived.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DisciplineComponent,
+    StatusComponent,
+    AchivedComponent
   ],
   imports: [
     BrowserModule
